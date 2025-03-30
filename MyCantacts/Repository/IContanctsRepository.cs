51e645c7fa24f665cffdb97fd122af9075c2d898
Isatis <Isatis@DESKTOP-LA911MV>
2025-03-30 05:22:38 +0330

    Add project files.

---
+++ MyCantacts/Repository/IContanctsRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace MyCantacts
+{
+    interface IContanctsRepository
+    {
+        DataTable SelectAll();
+        DataTable SelectRow(int contactId);
+        DataTable search(string parameter);
+        bool Insert(string name, string family, string mobile, string email, int age, string address);
+        bool Update(int contactID, string name, string family, string mobile, string email, int age, string address);
+        bool Delete(int contactID);
+        
+    }
+}
---
+++ MyCantacts/Services/ContactsRepository.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace MyCantacts.Services
+{
+    class ContactsRepository : IContanctsRepository
+    {
+        private string connectionString = "Data Source=DESKTOP-LA911MV\\MSY2020;Initial Catalog=Contact_DB;Integrated Security=true";
+
+        public bool Delete(int contactID)
+        {
+            SqlConnection connection =new SqlConnection(connectionString);
+            try
+            {
+                string query = "Delete From MyContacts where ContactID=@ID";
+                SqlCommand command=new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@ID",contactID);
+                connection .Open();
+                command.ExecuteNonQuery();
+                return true;
+            }
+            catch
+            {
+                return false;
+                
+            }
+            finally
+            {
+                connection.Close();
+
+            }
+        }
+
+        public bool Insert(string name, string family, string mobile, string email, int age, string address)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                string query = "Insert Into MyContacts (Name,Family,Email,Age,Mobile,Address) values (@Name,@Family,@Email,@Age,@Mobile,@Address)";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Name", name);
+                command.Parameters.AddWithValue("@Family", family);
+                command.Parameters.AddWithValue("@Email", email);
+                command.Parameters.AddWithValue("@Age", age);
+                command.Parameters.AddWithValue("@Mobile", mobile);
+                command.Parameters.AddWithValue("@Address", address);
+                connection.Open();
+                command.ExecuteNonQuery();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        public DataTable search(string parameter)
+        {
+            string query = "Select * From MyCantacts where Name like @parameter or Family like @parameter";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+            adapter.SelectCommand.Parameters.AddWithValue("@parameter", "%" + parameter + "%");
+            DataTable data = new DataTable();
+            adapter.Fill(data);
+            return data;
+        }
+
+        public DataTable SelectAll()
+        {
+            string query = "Select * From MyCantacts";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+            DataTable data = new DataTable();
+            adapter.Fill(data);
+            return data;
+        }
+
+        public DataTable SelectRow(int contactId)
+        {
+            string query = "Select * From MyCantacts where contactID="+contactId;
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+            DataTable data = new DataTable();
+            adapter.Fill(data);
+            return data;
+        }
+
+        public bool Update(int contactID, string name, string family, string mobile, string email, int age, string address)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                string query = "Update MyContacts (Name,Family,Email,Age,Mobile,Address) values (@Name,@Family,@Email,@Age,@Mobile,@Address)";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Name", name);
+                command.Parameters.AddWithValue("@Family", family);
+                command.Parameters.AddWithValue("@Email", email);
+                command.Parameters.AddWithValue("@Age", age);
+                command.Parameters.AddWithValue("@Mobile", mobile);
+                command.Parameters.AddWithValue("@Address", address);
+                connection.Open();
+                command.ExecuteNonQuery();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
